0f459852803fbd0c1d95aa7b69c69436df10b681
Drjjsplace <thejackslawgroup@gmail.com>
2025-08-31 10:55:06 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'https://www.mylawmanager.com'
+            value: '*'
           },
           {
             key: 'Access-Control-Allow-Methods',

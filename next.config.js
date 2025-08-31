631a09c00e03d211b41e43c1d151f39b6c40fe9a
Drjjsplace <thejackslawgroup@gmail.com>
2025-08-31 09:53:29 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/api/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://www.mylawmanager.com'
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Authorization, Content-Type'
+          }
+        ]
+      }
+    ];
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['pg']
+  }
+}
+
+module.exports = nextConfig
